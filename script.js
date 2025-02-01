58b611f34055f54fe1baa7cd76f5189c143dc247
Farman71 <farmanhussain71@gmail.com>
2025-02-01 15:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
-//your JS code here. If required.
-let form=document.getElementById("voteForm")
-form.addEventListener("submit",function(event){
-event.preventDefault();
-let age=document.getElementById("age").value.trim();
-let name=document.getElementById("name").value.trim();
-age=parseInt(age,10);
-let myPromise= new Promise((resolve,reject)=>{
-	if(age>18)
-	{
-		setTimeout(()=>{
-			resolve(`Welcome, ${name} . You can Vote.`);
-		},4000);
-	}
-	else
-	{
-		setTimeout(()=>{
-			reject(`Oh Sorry, ${name} . You aren't old enough.`);
-		},4000);
-		
-	}
-});
+let form = document.getElementById("voteForm");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  let age = document.getElementById("age").value.trim();
+  let name = document.getElementById("name").value.trim();
+  age = parseInt(age, 10);
+
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry, ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+
   myPromise
     .then((message) => {
       alert(message);
